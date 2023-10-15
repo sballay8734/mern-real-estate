a217eea3e2f0ac3dbe4cade5364a32541d0c4011
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-10-15 12:35:07 -0400

    Pre mongoose implementation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  // testing
   <React.StrictMode>
     <App />
   </React.StrictMode>,
--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req: any, res: any) => {
+  // test
   console.log("Port running")
 })
 
