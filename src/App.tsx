3d40a879fa24bf0c2477485fabd33b44a9f9c8c9
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-10-15 08:56:57 -0400

    Initial pages and route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import './App.scss'
 // import React from 'react'
+import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/Signin'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+
 
 export default function App() {
-  const age: number = 20
-  console.log(age)
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
